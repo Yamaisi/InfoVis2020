0befb9bc8c139117ceeb0c20b165f2d52fcea133
Yamaisi <65213211+Yamaisi@users.noreply.github.com>
2020-05-13 17:13:47 +0900

    task2 commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,19 +1,37 @@
-Vec3 = function( x, y, z )
+class Vec3
 {
-	this.x = x;
-	this.y = y;
-	this.z = z;
-}
+	constructor( x, y, z )
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 
-Vec3.prototype.add = function( v )
-{
-	this.x += v.x;
-	this.y += v.y;
-	this.z += v.z;
-	return this;
-}
+	add( v )
+	{
+		this.x += v.x;
+		this.y += v.y;
+		this.z += v.z;
+	}
 
-Vec3.prototype.sum = function()
-{
-	return this.x + this.y + this.z;
+	sum()
+	{
+		return this.x + this.y +this.z;
+	}
+
+	min(v)
+	{
+		return Math.min(v)
+	}
+
+	max(v)
+	{
+		return Math.max(v)
+	}
+
+	mid(v)
+	{
+		return Math.median(v)
+	}
+
 }
